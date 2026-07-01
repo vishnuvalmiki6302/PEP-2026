ef9a4179d2c5740e8b4a9a996a45e60cbba89cfb
vishnuvalmiki6302 <varh6302@gmail.com>
2026-07-01 11:27:30 +0530

    Strings

--- DSA/Strings/two.cpp
+++ DSA/Strings/two.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string revString(string s){
+ 
+    int i=0;
+    int j=s.length()-1;
+    while(i<j){
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+    return s;
+}
+
 int main(){
-    
+   string s;
+   cin>>s;
+   string rev=revString(s);
+   cout<<rev<<endl;
+  
 
     
 
